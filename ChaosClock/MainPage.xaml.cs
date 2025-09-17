08197d3727d3a8966460491e79f33c3100fc8e42
S00201014-ATU <S00201014@atu.ie>
2025-09-17 10:55:56 +0100

    Added Roman numeral clock display mode with toggle button

--- ChaosClock/MainPage.xaml.cs
+++ ChaosClock/MainPage.xaml.cs
@@ -5,7 +5,7 @@ namespace ChaosClock;
 public partial class MainPage : ContentPage
 {
     private readonly System.Timers.Timer timer;
-    private bool binaryMode = false;
+    private int modeIndex = 0;
 
     public MainPage()
     {
@@ -16,20 +16,36 @@ public partial class MainPage : ContentPage
         timer.Start();
     }
 
-    private void UpdateClock(object? sender, System.Timers.ElapsedEventArgs e)
+    private void UpdateClock(object? sender, ElapsedEventArgs e)
     {
         MainThread.BeginInvokeOnMainThread(() =>
         {
-            ClockLabel.Text = binaryMode
-            ? ConvertToBinary(DateTime.Now)
-            : DateTime.Now.ToString("HH:mm:ss");
+            if (modeIndex == 1)
+            {
+                ClockLabel.Text = ConvertToBinary(DateTime.Now);
+            }
+            else if (modeIndex == 2)
+            {
+                ClockLabel.Text = ConvertToRomanNumerals(DateTime.Now);
+            }
+            else
+            {
+                ClockLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+            }
         });
     }
 
-    private void onToggleBinaryClicked(object? sender, EventArgs e)
+    private void OnSwitchModeClicked(object sender, EventArgs e)
     {
-        binaryMode = !binaryMode;
-        ToggleButton.Text = binaryMode ? "Show Normal" : "Show Binary";
+        modeIndex = (modeIndex + 1) % 3;
+
+        ModeButton.Text = modeIndex switch
+        {
+            0 => "Switch to Binary",
+            1 => "Switch to Roman Numerals",
+            2 => "Switch to Normal",
+            _ => "Switch Mode"
+        };
     }
 
     private string ConvertToBinary(DateTime time)
@@ -39,5 +55,34 @@ public partial class MainPage : ContentPage
         string seconds = Convert.ToString(time.Second, 2).PadLeft(6, '0');
 
         return $"{hours}:{minutes}:{seconds}";
+    }
+
+    private string ConvertToRomanNumerals(DateTime time)
+    {
+        return $"{ToRoman(time.Hour)}:{ToRoman(time.Minute)}:{ToRoman(time.Second)}";
+    }
+
+    private string ToRoman(int number)
+    {
+        if (number == 0) return "N";
+
+        var map = new Dictionary<int, string>
+        {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+            {1, "I"}
+        };
+
+        var roman = "";
+        foreach (var kvp in map)
+        {
+            while (number >= kvp.Key)
+            {
+                roman += kvp.Value;
+                number -= kvp.Key;
+            }
+        }
+        return roman;
     }
 }
