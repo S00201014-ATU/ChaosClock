cb1a5c19e08685c3332bccdeb33990ef37792ba5
S00201014-ATU <S00201014@atu.ie>
2025-09-17 10:22:42 +0100

    Added binary clock display mode with toggle button

--- ChaosClock/MainPage.xaml.cs
+++ ChaosClock/MainPage.xaml.cs
@@ -5,6 +5,7 @@ namespace ChaosClock;
 public partial class MainPage : ContentPage
 {
     private readonly System.Timers.Timer timer;
+    private bool binaryMode = false;
 
     public MainPage()
     {
@@ -19,7 +20,24 @@ public partial class MainPage : ContentPage
     {
         MainThread.BeginInvokeOnMainThread(() =>
         {
-            ClockLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+            ClockLabel.Text = binaryMode
+            ? ConvertToBinary(DateTime.Now)
+            : DateTime.Now.ToString("HH:mm:ss");
         });
+    }
+
+    private void onToggleBinaryClicked(object? sender, EventArgs e)
+    {
+        binaryMode = !binaryMode;
+        ToggleButton.Text = binaryMode ? "Show Normal" : "Show Binary";
+    }
+
+    private string ConvertToBinary(DateTime time)
+    {
+        string hours = Convert.ToString(time.Hour, 2).PadLeft(6, '0');
+        string minutes = Convert.ToString(time.Minute, 2).PadLeft(6, '0');
+        string seconds = Convert.ToString(time.Second, 2).PadLeft(6, '0');
+
+        return $"{hours}:{minutes}:{seconds}";
     }
 }
